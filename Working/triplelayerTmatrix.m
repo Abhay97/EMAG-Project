a82ee27fe73ca65207eb66d75d0cb0470edc05e0
Abhay Mahajan <amaha102@uottawa.ca>
2017-12-04 15:01:45 -0500

    t-matrix, almost done

--- Working/triplelayerTmatrix.m
+++ Working/triplelayerTmatrix.m
@@ -1,20 +1,23 @@
-syms nAIR N1 N2 nSolar Lambda LambdaC
+syms nAIR N1 N2 N3 nSolar Lambda LambdaC
 
 
 %reflection coeffs - gamma
 r01 = (nAIR - N1)/(nAIR + N1);
 r12 = (N1 - N2)/(N1 + N2);
-r2S = (N2 - nSolar)/(N2 + nSolar); % to solar cell
+r23 = (N2 - N3)/(N2 + N3); %
+r3S = (N3 - nSolar)/(N3 + nSolar); % to solar cell
 
 %transmission coeffs - tau
 t01 = 2*(nAIR)/(nAIR +N1);
 t12 = 2*(N1)/(N1 +N2);
-t2S = 2*(N2)/(N2 +nSolar);
+t23 = 2*(N2)/(N2 +N3);
+t3S = 2*(N3)/(N3 +nSolar);
 
 %%Q Matrix
 Q01 = (1/t01)*([1 r01; r01 1]);
 Q12 = (1/t12)*([1 r12; r12 1]);
-Q2S = (1/t2S)*([1 r2S; r2S 1]);
+Q23 = (1/t23)*([1 r23; r23 1]);
+Q3S = (1/t3S)*([1 r3S; r3S 1]);
 
 %%%Design parameters%%%
 lambdaC = 650;  %nm centre wavelength
@@ -28,14 +31,14 @@ Delta3 = (pi/2)*(Lambda/LambdaC);
 
 P1 = [exp(j*Delta1) 0 ; 0 exp(-j*Delta1)];
 P2 = [exp(j*Delta2) 0 ; 0 exp(-j*Delta2)];
+P3 = [exp(j*Delta3) 0 ; 0 exp(-j*Delta3)];
 
 
 
-
-pT1= [1 -r01; r01 -1]*[1 -r12; r12 -1]*[1 r23; r23 1];
+pT1= [1 -r01; r01 -1]*[1 -r12; r12 -1]*[1 -r23; r23 -1]*[1 r3S; r3S 1];
 
 eqn = pT1(2,1) == 0;
 sol = solve(eqn, N2);
 
 disp('N2 = ')
-disp(sol(1))
+disp(sol(1));
--- t2.m
+++ t2.m
@@ -1,6 +1,7 @@
 syms nAIR N1 N2 nSolar Lambda LambdaC
 
-%REFLECTION coeffs
+
+%reflection coeffs - gamma
 r01 = (nAIR - N1)/(nAIR + N1);
 r12 = (N1 - N2)/(N1 + N2);
 r2S = (N2 - nSolar)/(N2 + nSolar); % to solar cell
@@ -15,15 +16,26 @@ Q01 = (1/t01)*([1 r01; r01 1]);
 Q12 = (1/t12)*([1 r12; r12 1]);
 Q2S = (1/t2S)*([1 r2S; r2S 1]);
 
-%Deltas
+%%%Design parameters%%%
+lambdaC = 650;  %nm centre wavelength
+Lthick = lambdaC/4; %
+
+%%Deltas
 Delta1 = (pi/2)*(Lambda/LambdaC);
 Delta2 = (pi/2)*(Lambda/LambdaC);
+Delta3 = (pi/2)*(Lambda/LambdaC);
 
-%P matrix
 
 P1 = [exp(j*Delta1) 0 ; 0 exp(-j*Delta1)];
 P2 = [exp(j*Delta2) 0 ; 0 exp(-j*Delta2)];
 
-pT1= [1 -r01; r01 -1]*[1 -r12; r12 -1]*[1 r2S ; r2S 1]
-pT1(2,1)
-exn
+
+
+
+pT1= [1 -r01; r01 -1]*[1 -r12; r12 -1]*[1 r23; r23 1];
+
+eqn = pT1(2,1) == 0;
+sol = solve(eqn, N2);
+
+disp('N2 = ')
+disp(sol(1))
